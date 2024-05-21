6d1d70cc65be0d57367666381405d45f565af027
t-zuntong.ming <t-zuntong.ming@pictc.com>
2024-05-21 14:55:00 +0800

    feat: 新增配置

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,12 +1,37 @@
+/*
+ * @Author: t-zuntong.ming t-zuntong.ming@pictc.com
+ * @Date: 2024-05-21 10:48:45
+ * @LastEditors: t-zuntong.ming t-zuntong.ming@pictc.com
+ * @LastEditTime: 2024-05-21 13:44:07
+ * @FilePath: \vue-project\commitlint.config.cjs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 module.exports = {
-  ignores: [(commit) => commit.includes('init')],
-  extends: ['@commitlint/config-conventional'],
+  extends: ["@commitlint/config-conventional"],
+  // 校验规则
   rules: {
-    'body-leading-blank': [2, 'always'],
-    'footer-leading-blank': [1, 'always'],
-    'header-max-length': [2, 'always', 108],
-    'subject-empty': [2, 'never'],
-    'type-empty': [2, 'never'],
-    'subject-case': [0],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "chore",
+        "revert",
+        "build",
+      ],
+    ],
+    "type-case": [0],
+    "type-empty": [0],
+    "scope-empty": [0],
+    "scope-case": [0],
+    "subject-full-stop": [0, "never"],
+    "subject-case": [0, "never"],
+    "header-max-length": [0, "always", 72],
   },
-}
+};
--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
 let a = "1";
 </script>
 
@@ -15,8 +14,6 @@ let a = "1";
     />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
